24564a2ea69978d40e37026dbb2acb5947c31087
danielan <daniel.an92@gmail.com>
2021-01-21 16:40:57 -0500

    remove app id and key from recipe

--- commands/advanced/recipe.js
+++ commands/advanced/recipe.js
@@ -21,7 +21,7 @@ module.exports = class RecipeCommand extends Command {
     }
 
     async run(message, { search }) {
-        const res = await (await fetch(`https://api.edamam.com/search?q=${search}&app_id=d6f1385f&app_key=8bad6a76df3a71d7717bc9e0cc285d9e&from=0&to=3`)).json();
+        const res = await (await fetch(`https://api.edamam.com/search?q=${search}&app_id=<INSERT APP ID>&app_key=<INSERT APP KEY>&from=0&to=3`)).json();
         for (const hit of res.hits) {
             const recipe = hit.recipe;
             const embed = new MessageEmbed()
